827d03446fb57380d45618d791a171b13c8e05de
Ryan Zidago <ryanzidago@gmail.com>
2021-03-18 20:34:26 +0100

    added implace mutation of hashmap

--- single_number/src/main.rs
+++ single_number/src/main.rs
@@ -23,6 +23,22 @@ fn single_number_00(nums: Vec<i32>) -> i32 {
     unreachable!()
 }
 
+fn single_number_00_bis(nums: Vec<i32>) -> i32 {
+    let mut hashmap: HashMap<i32, i32> = HashMap::new();
+
+    for n in nums.iter() {
+        hashmap.entry(*n).and_modify(|m| *m += 1).or_insert(1);
+    }
+
+    for (key, value) in hashmap {
+        if value == 1 {
+            return key;
+        }
+    }
+
+    unreachable!()
+}
+
 fn single_number_01(nums: Vec<i32>) -> i32 {
     let mut hashmap: HashMap<i32, i32> = HashMap::new();
 
@@ -50,6 +66,18 @@ mod test {
 
         let input = vec![1];
         assert_eq!(single_number_00(input), 1);
+    }
+
+    #[test]
+    fn test_name() {
+        let input = vec![2, 2, 1];
+        assert_eq!(single_number_00_bis(input), 1);
+
+        let input = vec![4, 1, 2, 1, 2];
+        assert_eq!(single_number_00_bis(input), 4);
+
+        let input = vec![1];
+        assert_eq!(single_number_00_bis(input), 1);
     }
 
     #[test]
