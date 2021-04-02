4904dbe21a98f55502fe8d63fa1c4c833e0c82df
Ryan Zidago <ryanzidago@gmail.com>
2021-04-02 15:33:55 +0200

    added brute force solution

--- best-time-to-buy-and-sell-stocks-1/src/main.rs
+++ best-time-to-buy-and-sell-stocks-1/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn max_profit(prices: Vec<i32>) -> i32 {
+fn kadane(prices: Vec<i32>) -> i32 {
     let mut current_sum: i32 = 0;
     let mut best_sum: i32 = 0;
 
@@ -17,14 +17,37 @@ fn max_profit(prices: Vec<i32>) -> i32 {
     return best_sum;
 }
 
+fn brute_force(prices: Vec<i32>) -> i32 {
+    let mut max_profit: i32 = 0;
+
+    for i in 0..(prices.len() - 1) {
+        for j in (i + 1)..prices.len() {
+            let profit: i32 = prices[j] - prices[i];
+
+            if profit > max_profit {
+                max_profit = profit;
+            }
+        }
+    }
+
+    return max_profit;
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn max_profit_test() {
-        assert_eq!(5, max_profit(vec![7, 1, 5, 3, 6, 4]));
-        assert_eq!(1, max_profit(vec![1, 2]));
-        assert_eq!(0, max_profit(vec![5, 4, 3, 2, 1]));
+    fn kadane_test() {
+        assert_eq!(5, kadane(vec![7, 1, 5, 3, 6, 4]));
+        assert_eq!(1, kadane(vec![1, 2]));
+        assert_eq!(0, kadane(vec![5, 4, 3, 2, 1]));
+    }
+
+    #[test]
+    fn brute_force_test() {
+        assert_eq!(5, brute_force(vec![7, 1, 5, 3, 6, 4]));
+        assert_eq!(1, brute_force(vec![1, 2]));
+        assert_eq!(0, brute_force(vec![5, 4, 3, 2, 1]));
     }
 }
